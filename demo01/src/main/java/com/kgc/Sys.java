28cb41072cfb48dd2d78d97cbad7dc6856058b40
wangyifan0000 <2893084319>
2020-09-19 09:27:21 +0800

    分支提交

---
+++ demo01/src/main/java/com/kgc/Sys.java
@@ -0,0 +1,11 @@
+package com.kgc;
+
+/**
+ * @author wangyf
+ * @create 2020-09-19 9:09
+ */
+public class Sys {
+    public static void main(String[] args) {
+        System.out.println("团队开发，接受邀请，复制链接，两人保护能改同一个文件，谁创建谁修改");
+    }
+}
