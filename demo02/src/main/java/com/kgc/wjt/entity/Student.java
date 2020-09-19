2070c251565037d54fc52dbdade9cb57ea4dd6ad
86182 <3271397298@qq.com>
2020-09-19 09:34:48 +0800

    提交学生实体类

---
+++ demo02/src/main/java/com/kgc/wjt/entity/Student.java
@@ -0,0 +1,35 @@
+package com.kgc.wjt.entity;
+
+/**
+ * @author shkstart
+ * @create 2020-09-19 9:33
+ */
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
