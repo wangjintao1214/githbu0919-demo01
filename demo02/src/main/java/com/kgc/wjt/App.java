b2184973f0ef223f680f852b0831c9634db49858
86182 <3271397298@qq.com>
2020-09-19 09:35:05 +0800

    提交学生实体类

---
+++ demo02/src/main/java/com/kgc/wjt/App.java
@@ -0,0 +1,13 @@
+package com.kgc.wjt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
